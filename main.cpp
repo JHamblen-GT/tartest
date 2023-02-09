f43f2a192e246c197222905565b6fd9b2a3892f2
JHamblen-GT <hamblen@ece.gatech.edu>
2023-02-09 15:27:02 +0000

    v1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "uLCD_4DGL.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
